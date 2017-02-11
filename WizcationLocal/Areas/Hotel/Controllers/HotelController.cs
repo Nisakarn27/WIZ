8a3b05545d045f50d8362c3d9cd624023c1c11b3
nisakarn27 <karnlyn@gmail.com>
2017-02-11 17:26:06 +0700

    init

---
+++ WizcationLocal/Areas/Hotel/Controllers/HotelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WizcationLocal.Areas.Hotel.Controllers
+{
+    public class HotelController : Controller
+    {
+        // GET: Hotel/Hotel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WizcationLocal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WizcationLocal.Startup))]
+namespace WizcationLocal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
