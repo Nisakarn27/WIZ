679328587639b63a7d34e80adb1ef55db98d94d9
tengnuen_g <komsan@vtec-system.com>
2017-03-13 15:01:08 +0700

    1. add images 2. update home page 3. update angular ui-router 4. add searchDetails page

--- WizcationLocal/js/app.js
+++ WizcationLocal/js/app.js
@@ -1,15 +1,21 @@
 ﻿var app = angular.module('wizcation', ['ngSanitize', 'ui.router', 'angular-carousel', 'ui.bootstrap.datetimepicker']);
 
-app.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/")
+app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise("/");
+    $locationProvider.hashPrefix('');
     $stateProvider
         .state('/', {
             url: "/",
             templateUrl: '\Home/Home'
         })
-        .state('details', {
+        .state('/details', {
             url: '/details',
             templateUrl: '\Home/Details',
             controller: 'DetailController'
+        })
+        .state('/searchDetails', {
+            url: '/searchDetails',
+            templateUrl: '\Home/SearchDetails',
+            controller: 'SearchDetailsController'
         });
 });
