f60c56d22473300a3527e1bfef3600744495a3d3
tengnuen_g <komsan@vtec-system.com>
2017-03-25 11:20:51 +0700

    add git ignore

--- WizcationLocal/js/SearchDetails.js
+++ WizcationLocal/js/SearchDetails.js
@@ -1,3 +1,5 @@
 ﻿angular.module('wizcation').controller('SearchDetailsController', ['$scope', function ($scope) {
-    
+    $scope.seeDetals = function () {
+        parent.location = '#/details'
+    }
 }])
